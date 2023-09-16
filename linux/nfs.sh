9aec4aa1f05dc43f45801dcaac19e418c68316b9
Muyouming <muyouming@gmail.com>
2023-09-16 14:12:08 +0000

    uddatep

--- linux/nfs.sh
+++ linux/nfs.sh
@@ -3,3 +3,6 @@ sudo apt-get update
 sudo apt-get install nfs-common
 mkdir ~/nfs
 sudo mount -t nfs 192.168.3.17:/volume1/linux ~/nfs
+
+
+echo "192.168.3.17:/volume1/linux /homes/ubuntu/nfs nfs defaults 0 0" | sudo tee -a /etc/fstab
